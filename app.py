3b1cb07baa9397a5c749432eac0770d159985364
MinsandiDeSilva <minsandidesilva21@gmail.com>
2025-03-23 08:33:04 +0530

    Fixed deployment issues

--- app.py
+++ app.py
@@ -19,9 +19,22 @@
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=10000)
 
-from __init__ import create_app
+# from __init__ import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT if set, otherwise default to 5000
+    app.run(host="0.0.0.0", port=port)
